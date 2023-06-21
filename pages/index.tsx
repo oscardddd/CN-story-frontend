987317e357201a02d7f741716f4dedf987224016
oscarddddddd <qianlidong0@gmail.com>
2023-06-21 02:37:01 -0500

    upload img setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,76 @@
 import Page from '@/components/page'
 import Section from '@/components/section'
+import { useState } from 'react';
+import styles from './index.module.css'
+import axios from 'axios'
 
-const Index = () => (
-	<Page>
-		<Section>
-			<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-				We grow a lot of rice.
-			</h2>
 
-			<div className='mt-2'>
-				<p className='text-zinc-600 dark:text-zinc-400'>
-					You love rice, and so does the rest of the world. In the crop year
-					2008/2009, the milled rice production volume amounted to over{' '}
-					<span className='font-medium text-zinc-900 dark:text-zinc-50'>
-						448 million tons
-					</span>{' '}
-					worldwide.
-				</p>
+async function postImage({image, description}) {
+	const formData = new FormData();
+	formData.append("image", image)
+	formData.append("description", description)
+  
+	// const result = await fetch('http://localhost:4000/user/upload',
+	// {
+	// 	method:'post',
+	// 	body:formData,
+	// 	// headers:{'Content-Type': 'multipart/form-data'},
+	// })
+	const result = await axios.post('http://localhost:4000/user/upload', formData,
+	 )
 
-				<br />
+	console.log(result.data)
+	return result.data
+}
 
-				<p className='text-sm text-zinc-600 dark:text-zinc-400'>
-					<a
-						href='https://github.com/mvllow/next-pwa-template'
-						className='underline'
-					>
-						Source
-					</a>
-				</p>
+
+
+function Index() {
+
+	const [file, setFile] = useState()
+	const [description, setDescription] = useState("")
+	const [images, setImages] = useState([])
+  
+	const submit = async event => {
+	  event.preventDefault()
+	  const result = await postImage({image: file, description})
+	  setImages([result.image, ...images])
+	}
+  
+	const fileSelected = event => {
+	  const file = event.target.files[0]
+	  setFile(file)
+	}
+  
+	return (
+		<Page>
+  		<div className={styles.App}>
+			<form className={`${styles.test}`} onSubmit={submit}>
+				<input onChange={fileSelected} type="file" accept="image/*"></input>
+				<input value={description} placeholder='Any comments?' onChange={e => setDescription(e.target.value)} type="text"></input>
+				<button className={styles.button} type="submit">Submit</button>
+			</form>
+	
+			{ images.map( image => (
+			<div key={image}>
+				<img src={image}></img>
 			</div>
-		</Section>
-	</Page>
-)
+			))}
+  
+	  	</div>
+		</Page>
+	
+	);
+  }
+
+// const Index = () => (
+// 	<Page>
+// 		<Section>
+// 			<div className='box1'>
+// 				<button>Upload Image</button>
+// 			</div>
+// 		</Section>
+// 	</Page>
+// )
 
 export default Index
